d3e61b7b6451da5c193e460542dd0e21b68277a9
jackal998 <jackal998@gmail.com>
2016-11-09 20:08:40 +0800

    build_relations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
 	validates_presence_of :name
 
+	has_many :event_groupships
+	has_many :groups, :through => :event_groupships
+
 	has_many :attendees
 	has_one :location
 
