ec567900415b3a108737a2bf86380edb8731761e
jackal998 <jackal998@gmail.com>
2016-11-09 19:23:02 +0800

    first_6th_chapter

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,13 @@ before_action :set_event, :only => [:show, :edit, :update]
 
 	def index
 		@events = Event.page(params[:page]).per(5)
+
+		respond_to do |format|
+			format.html # index.html.erb
+			format.xml { render :xml => @events.to_xml }
+			format.json { render :json => @events.to_json }
+			format.atom { @feed_title = "My event list" } # index.atom.builder
+		end
 	end
 
 	def new
@@ -21,6 +28,11 @@ before_action :set_event, :only => [:show, :edit, :update]
 	end
 
 	def show
+		respond_to do |format|
+			format.html { @page_title = @event.name } # show.html.erb
+			format.xml # show.xml.builder
+			format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+		end
 	end
 
 	def edit
