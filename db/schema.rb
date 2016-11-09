b399191bd70eb7dd7380a618edd4b9cc0b367b7b
jackal998 <jackal998@gmail.com>
2016-11-09 19:28:46 +0800

    g model category/attendee

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109084232) do
+ActiveRecord::Schema.define(version: 20161109112400) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +26,8 @@ ActiveRecord::Schema.define(version: 20161109084232) do
     t.integer  "capacity"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_events_on_category_id"
   end
 
 end
