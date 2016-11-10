e291f961c781cc12fae69eefb8d014e298ad419e
jackal998 <jackal998@gmail.com>
2016-11-10 13:57:07 +0800

    到用 Nested Model 順帶編輯跟新增

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+
 	validates_presence_of :name
 
 	has_many :event_groupships
@@ -8,4 +9,7 @@ class Event < ApplicationRecord
 	has_one :location
 
 	belongs_to :category
+
+	accepts_nested_attributes_for :location, :allow_destroy => true, :reject_if => :all_blank
+
 end
