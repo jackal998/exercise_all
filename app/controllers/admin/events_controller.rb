be0bcd31688d4651047290e1adc4f6fae0d173c3
jackal998 <jackal998@gmail.com>
2016-11-11 19:17:23 +0800

    ch8_finish(add admin)

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,2 +1,16 @@
 class Admin::EventsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :check_admin
+  layout "admin"
+
+  # ....
+
+  protected
+
+  def check_admin
+    unless current_user.admin?
+      raise ActiveRecord::RecordNotFound
+    end
+  end
 end
